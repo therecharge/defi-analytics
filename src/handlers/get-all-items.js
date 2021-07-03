12e80a0c7466eff089b25a3e169141d5cfdbcc37
humblefirm <humblefirm@naver.com>
2021-07-04 04:46:41 +0900

    init

--- src/handlers/get-all-items.js
+++ src/handlers/get-all-items.js
@@ -1,36 +1,115 @@
-// Create clients and set shared const values outside of the handler
+const ethers = require("ethers");
+const Web3Utils = require("web3-utils");
+const { toWei, fromWei, BN } = Web3Utils;
+let totalCirculation = {
+  ERC: new BN(toWei("1000000000", "ether")),
+  HRC: new BN(toWei("1000000000", "ether")),
+};
+const rechargeWallet = {
+  ERC: ["0x3c2465d88C6546eac6F9aa6f79081Ad874CA2E8b"],
+  HRC: [
+    "0xb0bddd278C42F879A690bAfdc8301985C925b4f9",
+    "0xFabede8fE44589A08FbF13C4425FaEE8CEaD63c0",
+    "0x5eDf1BFA145d081bFafc85cc161f465E0788Eea7",
+    "0x6D07380155d00922d2003bB7E7D4b83291D2394B",
+    "0xC6EC1Bb3afDd25150db8F6c3aAEB2D407B2aBc17",
+    "0x65aa81Feab9e36C4B5d5443Fb25532771e540E64",
+    "0xE6C873acCE44746E7Dc918e3ac957bbE78d717EA",
+    "0x9fA218bB826a847999875e38E39a79B59C8E365b",
+    "0x8Ec4422a07c3404204221A074c00Cbb1667fFd4E",
+  ],
+};
+const tokenAddress = {
+  ERC: "0xe74bE071f3b62f6A4aC23cA68E5E2A39797A3c30",
+  HRC: "0xbddC276CACC18E9177B2f5CFb3BFb6eef491799b",
+};
+const provider = {
+  ERC: new ethers.providers.JsonRpcProvider(
+    "https://eth-mainnet.alchemyapi.io/v2/2wgBGtGnTm3s0A0o23RY0BtXxgow1GAn"
+  ),
+  HRC: new ethers.providers.JsonRpcProvider(
+    "https://http-mainnet.hecochain.com"
+  ),
+};
+const networks = ["ERC", "HRC"];
 
-// Create a DocumentClient that represents the query to get all items
-const dynamodb = require('aws-sdk/clients/dynamodb');
+const ERC20ABI = require("./abi/ERC20ABI.json");
 
-const docClient = new dynamodb.DocumentClient();
+const getBalance = async () => {
+  const res = await Promise.all(
+    networks.map(async (network) => {
+      const tokenI = new ethers.Contract(
+        tokenAddress[network],
+        ERC20ABI,
+        provider[network]
+      );
+      return Promise.all(
+        rechargeWallet[network].map(async (address) => {
+          return (await tokenI.balanceOf(address)).toString();
+        })
+      );
+    })
+  );
 
-// Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
+  res.map((balances, i) => {
+    balances.map((balance) => {
+      totalCirculation[networks[i]] = totalCirculation[networks[i]].sub(
+        new BN(balance)
+      );
+    });
+  });
+  return {
+    ERC: totalCirculation.ERC.toString(),
+    HRC: totalCirculation.HRC.toString(),
+  };
+};
 
-/**
- * A simple example includes a HTTP get method to get all items from a DynamoDB table.
- */
 exports.getAllItemsHandler = async (event) => {
-    const { httpMethod, path } = event;
-    if (httpMethod !== 'GET') {
-        throw new Error(`getAllItems only accept GET method, you tried: ${httpMethod}`);
-    }
-    // All log statements are written to CloudWatch by default. For more information, see
-    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-    console.log('received:', JSON.stringify(event));
+  const { httpMethod, path } = event;
+  if (httpMethod !== "GET") {
+    throw new Error(
+      `getAllItems only accept GET method, you tried: ${httpMethod}`
+    );
+  }
+  // All log statements are written to CloudWatch by default. For more information, see
+  // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
+  console.log("received:", JSON.stringify(event));
 
-    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
-    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
-    const params = { TableName: tableName };
-    const { Items } = await docClient.scan(params).promise();
+  const totalCirculation = await getBalance();
+  let ret = {
+    ERC: {
+      total: totalCirculation.ERC,
+      redemption: "0",
+      price: "1",
+      swapped: "0",
+      conversion: "0",
+    },
+    HRC: {
+      total: totalCirculation.HRC,
+      redemption: "0",
+      price: "1",
+      swapped: "0",
+      conversion: "0",
+    },
+    general: {
+      ServicesPlugged: 1,
+      ChargersActivated: 10,
+      BridgesActivated: 1,
+      RedemptionRate: 200,
+      ProposalsPosted: 1,
+      ProposalsApproved: 10,
+      ProposalsRejected: 1,
+      ProposalsPending: 10,
+    },
+  };
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(Items),
-    };
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(ret),
+  };
 
-    console.log(`response from: ${path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
+  console.log(
+    `response from: ${path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
+  return response;
 };
